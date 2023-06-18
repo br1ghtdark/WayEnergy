942b3d8313e3a341cf427f16c17860e666c25ea5
br1ghtdark <marsha1l15@yandex.ru>
2023-06-18 22:52:37 +0300

    burger menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 
+/*=========================Burger_menu========================*/
+
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+        $('.burger').addClass('burger--follow')
+    } else {
+        $('.burger').removeClass('burger--follow')
+    }
+})
+
+$('.burger, .burger--follow, .overlay').on('click', function (e) {
+e.preventDefault()
+$('.header__top').toggleClass('header__top--open')
+$('.overlay').toggleClass('overlay--show')
+$('.burger').toggleClass('burger--active')
+})
+
+/*=========================Burger_menu========================*/
+
 /* =============================AVAILABILITY-SLIDER============================ */
 /* =============================AVAILABILITY-SLIDER============================ */
 
